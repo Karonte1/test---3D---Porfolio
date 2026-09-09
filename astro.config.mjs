1769ecf7635a09474bc3bad36a18a124188ea745
karonte1 <duquejuanjose19@gmail.com>
2026-09-09 18:35:02 +0200

    activar despligue automatico de github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://tu-usuario.github.io',
+  base: '/tu-repositorio/', // ¡Ojo con las barras '/' al principio y al final!
+});
